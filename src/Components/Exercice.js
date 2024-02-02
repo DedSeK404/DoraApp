7048ed731669614c86e1c1763ec84ae5e2a31594
DedSeK404 <marwenbennaceur5@gmail.com>
2024-02-02 23:20:02 +0100

    second commit

--- src/Components/Exercice.js
+++ src/Components/Exercice.js
@@ -4,6 +4,7 @@ import doraBonjour from "./Videos/bonjours mes amis.mp4";
 import doraCommentCava from "./Videos/Comment ça va.mp4";
 import doraAujourdhui from "./Videos/aujourdhui.mp4";
 import doraReviser from "./Videos/nous allons reviser.mp4";
+import { Link } from "react-router-dom";
 import "react-html5video/dist/styles.css";
 
 const Exercice = () => {
@@ -17,11 +18,13 @@ const Exercice = () => {
   ];
 
   const handleSegment = () => {
-    console.log(videoSegments[segment]);
     setSegment((prevIndex) => (prevIndex + 1) % videoSegments.length);
     setIsVideoFinished(false);
   };
-  console.log(segment);
+  const handlePreviousSegment = () => {
+    setSegment((prevIndex) => (prevIndex - 1) % videoSegments.length);
+    setIsVideoFinished(false);
+  };
 
   const [isVideoFinished, setIsVideoFinished] = useState(false);
 
@@ -43,27 +46,51 @@ const Exercice = () => {
       console.error("Fullscreen not supported by this browser");
     }
   };
+  const [audio] = useState(new Audio("/Happy Upbeat Background Music.mp3"));
+  const [muteAudio, setMuteAudio] = useState(true);
+  useEffect(() => {
+    audio.loop = true;
+    audio.volume = 0.1;
+    audio.play();
+    return () => audio.pause(); // Cleanup when component unmounts
+  }, [audio]);
+  const handleMuteAudio = () => {
+    setMuteAudio(!muteAudio);
+    muteAudio ? setMuteAudio(audio.pause()) : audio.play();
+  };
+
   return (
     <div>
-      <button
-        style={{
-          position: "absolute",
-          zIndex: "2",
-        }}
+      <img
         onClick={handleSegment}
-      >
-        Next slide
-      </button>
-      <button
-        style={{
-          position: "absolute",
-          zIndex: "2",
-          marginLeft: "50%",
-        }}
+        className="nextButton"
+        src="/next.svg"
+        alt="0"
+      />
+
+      <img
+        onClick={handlePreviousSegment}
+        className="previousButton"
+        src="/previous.svg"
+        alt="0"
+      />
+
+      <img
+        onClick={handleMuteAudio}
+        className="audioButton"
+        src={!muteAudio ? "/muteAudio.svg" : "/playAudio.svg"}
+        alt="0"
+      />
+
+      <img
         onClick={handleFullscreen}
-      >
-        Toggle Fullscreen
-      </button>
+        className="fullscreenButton"
+        src="/fullscreen.svg"
+        alt="0"
+      />
+      <Link to="/">
+        <img className="homeButton" src="/homeButton.svg" alt="0" />
+      </Link>
       <div style={{ width: "100%", height: "100vh" }}>
         {!isVideoFinished ? (
           <video
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,17 +1,53 @@
-import React from 'react'
+import React, { useRef } from "react";
 import { Link } from "react-router-dom";
+import intro from "./Videos/intro.mp4";
+import "./Exercice.css";
 
 const Home = () => {
+  const handleFullscreen = () => {
+    const element = document.documentElement;
+
+    if (document.fullscreenEnabled) {
+      if (!document.fullscreenElement) {
+        element.requestFullscreen().catch((err) => {
+          console.error("Error attempting to enable fullscreen:", err);
+        });
+      } else {
+        document.exitFullscreen();
+      }
+    } else {
+      console.error("Fullscreen not supported by this browser");
+    }
+  };
+  const videoRef = useRef(null);
+
+  const handleMouseEnter = () => {
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
+  };
+
   return (
-    <div>
-    <h1>Home page</h1>
-
-    <Link to="/exercice">
-    <button>Commencer</button>
-            </Link>
-    
+    <div onClick={handleMouseEnter}>
+      <Link to="/exercice">
+        <img className="playButton" src="/playButton.svg" alt="0" />
+      </Link>
+      <img
+        onClick={handleFullscreen}
+        className="fullscreenButton"
+        src="/fullscreen.svg"
+        alt="0"
+      />
+      <video
+        ref={videoRef}
+        className="introVideo"
+        src={intro}
+        autoPlay
+        loop
+       
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
